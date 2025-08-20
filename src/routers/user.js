40ae224f70cb396a27c094569e5862f6a764a139
Godson Saji <godsonsaji077@gmail.com>
2025-08-20 15:00:35 +0530

    update files

--- src/routers/user.js
+++ src/routers/user.js
@@ -3,17 +3,47 @@ const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
 const userRouter = express.Router();
 
+const USER_SAFE_DATA = "firstName lastName age gender profileImage";
+
 userRouter.get("/user/requests", userAuth, async (req, res) => {
   try {
     const loggedInUser = req.user;
 
     const connectionRequests = await ConnectionRequest.find({
       toUserId: loggedInUser._id,
-    }).populate("fromUserId", ["firstName", "lastName"]);
+    }).populate("fromUserId", USER_SAFE_DATA);
     res.json({
       message: "Data Fetched Successfully",
       data: connectionRequests,
     });
+  } catch (error) {
+    res.status(400).send(`ERROR: ${error.message}`);
+  }
+});
+
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    const connectionRequests = await ConnectionRequest.find({
+      $or: [
+        { fromUserId: loggedInUser._id, status: "accept-match" },
+        { toUserId: loggedInUser._id, status: "accept-match" },
+      ],
+    })
+      .populate("fromUserId", USER_SAFE_DATA)
+      .populate("toUserId", USER_SAFE_DATA);
+
+    console.log(connectionRequests);
+
+    const data = connectionRequests.map((row) => {
+      if (row.fromUserId._id.toString() === loggedInUser._id.toString()) {
+        return row.toUserId._id;
+      }
+      return row.fromUserId;
+    });
+
+    res.json({ data });
   } catch (error) {
     res.status(400).send(`ERROR: ${error.message}`);
   }
